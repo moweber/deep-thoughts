96aa07ff248f587b7da514d2bc41d0fffc0cd4c5
DESKTOP-NK6EJSL\moweber <moworldenterprises@gmail.com>
2022-10-23 20:26:55 -0500

    login/register functionality added. profile page setup.

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Auth from "../../utils/auth";
+
+const logout = (event) => {
+  event.preventDefault();
+  Auth.logout();
+};
+
 const Header = () => {
   return (
     <header className="bg-secondary mb-4 py-2 flex-row align-center">
@@ -9,8 +16,19 @@ const Header = () => {
           <h1>Deep Thoughts</h1>
         </Link>
         <nav className="text-center">
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Signup</Link>
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
         </nav>
       </div>
     </header>
