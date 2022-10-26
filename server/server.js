44b8b85f589adadc3a858217806e78025628dbed
Matt Weber <matt@awebersite.com>
2022-10-26 14:39:54 -0500

    testing.

--- server/server.js
+++ server/server.js
@@ -14,6 +14,7 @@ const { authMiddleware } = require("./utils/auth");
 const db = require("./config/connection");
 
 // define fallback port
+console.log(process.env);
 const PORT = process.env.PORT || 3001;
 
 // create a new Apollo server and pass in our schema data
